7fb0b02f96215a5fb84129c362a0b375f063c566
Usman Rashid <usman.rasheed@algoace.com>
2023-02-06 21:35:35 +0500

    UI changed and Last entry is deletable now

--- src/screens/groceryScreen/groceryScreen.js
+++ src/screens/groceryScreen/groceryScreen.js
@@ -1,275 +1,3 @@
-// import React, {useEffect} from 'react';
-// import {
-//   View,
-//   Text,
-//   SafeAreaView,
-//   FlatList,
-//   TextInput,
-//   Alert,
-//   KeyboardAvoidingView,
-//   Pressable,
-// } from 'react-native';
-// import styles from './styles';
-// import {TextButton} from '../../components/textButton';
-// import AsyncStorage from '@react-native-async-storage/async-storage';
-
-// let dummyData = [
-//   {
-//     date: '',
-//     product: '',
-//     price: 0,
-//   },
-// ];
-
-// const GroceryScreen = ({navigation}) => {
-//   console.log('Screen Rendered');
-
-//   var date = new Date().getDate();
-//   var month = new Date().getMonth() + 1;
-//   var year = new Date().getFullYear();
-//   const currentDate = `${date}/${month}/${year}`;
-//   // console.warn(currentDate);
-//   const [products, setProducts] = React.useState([]);
-//   const [product, setProduct] = React.useState('');
-//   const [price, setPrice] = React.useState('');
-//   const [copyData, setCopyData] = React.useState(null);
-//   const [total, setTotal] = React.useState(0);
-
-//   React.useEffect(() => {
-//     const getTotal = async () => {
-//       try {
-//         const storedTotal = await AsyncStorage.getItem('total');
-//         setTotal(Number(storedTotal));
-//         console.log('Total received from async: ', total);
-//       } catch (error) {
-//         console.error(error);
-//       }
-//     };
-
-//     getTotal(); // Get the total from async storage
-//   }, []);
-
-//   React.useEffect(() => {
-//     const getStoredProducts = async () => {
-//       try {
-//         const storedProducts =
-//           JSON.parse(await AsyncStorage.getItem('products')) || [];
-//         setProducts(storedProducts);
-//       } catch (error) {
-//         console.error(error);
-//       }
-//     };
-
-//     getStoredProducts();
-
-//     try {
-//       const total = AsyncStorage.getItem('total');
-//       console.log('ITEM Get:', total.toString());
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   }, []);
-//   // const getTotal = async () => {
-//   //   try {
-//   //     const total = await AsyncStorage.getItem("total");
-//   //     return total;
-//   //   } catch (error) {
-//   //     console.error(error);
-//   //   }
-//   // };
-//   const renderHeader = () => {
-//     return (
-//       <View style={styles.headerStyle}>
-//         <Text>Grocery List</Text>
-//       </View>
-//     );
-//   };
-
-//   const renderGroceryList = () => {
-//     return (
-//       <View style={styles.groceryListContainer}>
-//         <View
-//           style={{
-//             flexDirection: 'row',
-//             justifyContent: 'space-evenly',
-//             width: '100%',
-//             marginTop: 10,
-//           }}>
-//           <Text style={{fontSize: 16, fontWeight: 'bold'}}>DATE</Text>
-//           <Text style={{fontSize: 16, fontWeight: 'bold'}}>PRODUCT</Text>
-//           <Text style={{fontSize: 16, fontWeight: 'bold'}}>PRICE</Text>
-//         </View>
-//         <FlatList
-//           data={products[0]}
-//           style={{width: '100%', marginTop: 10}}
-//           ListHeaderComponent={() => {
-//             //View to set in Header
-//             return (
-//               <View
-//                 style={{
-//                   flexDirection: 'row',
-//                   width: '100%',
-//                   justifyContent: 'space-around',
-//                   marginBottom: 10,
-//                   alignItems: 'center',
-//                 }}></View>
-//             );
-//           }}
-//           keyExtractor={item => item.id}
-//           renderItem={({item}) => {
-//             return (
-//               <Pressable
-//                 style={{
-//                   flexDirection: 'row',
-//                   justifyContent: 'space-around',
-//                   alignItems: 'center',
-//                   paddingBottom: 5,
-//                 }}
-//                 onLongPress={() =>
-//                   Alert.alert(
-//                     'Delete?',
-//                     `${item.product}-${item.price}-${item.id}`,
-//                     [
-//                       {
-//                         text: 'Cancel',
-
-//                         style: 'destructive',
-//                       },
-//                       {
-//                         text: 'OK',
-//                         onPress: () => console.warn('S'),
-//                       },
-//                     ],
-//                   )
-//                 }>
-//                 <Text>{item.date}</Text>
-//                 <Text>{item.product}</Text>
-//                 <Text>{item.price}</Text>
-//               </Pressable>
-//             );
-//           }}
-//         />
-//       </View>
-//     );
-//   };
-
-//   useEffect(() => {
-//     console.log('current TOTAL before Set State is: ', total);
-//     setTotal(total);
-//     console.log('current TOTAL after Set State is: ', total);
-//     saveTotal(total);
-//   }, [total]);
-//   const saveTotal = async total => {
-//     console.log('Save Total Called');
-//     try {
-//       await AsyncStorage.setItem('total', total.toString());
-//       console.log('Total saved', total);
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   };
-//   // const makePDF = () => {
-//   //   return (
-//   //     <Document>
-//   //       <Page>
-//   //         {products.map(item => (
-//   //           <Text>{`Date: ${item.date} Product: ${item.product} Price: ${item.price}`}</Text>
-//   //         ))}
-//   //       </Page>
-//   //     </Document>
-//   //   );
-//   // };
-//   const addToList = async () => {
-//     // console.log("Data to be added is: ", currentDate, product, price);
-//     const newProduct = {date: currentDate, product: product, price: price};
-//     if (!product) {
-//       Alert.alert('Please Enter Product');
-//     } else if (!price) {
-//       Alert.alert('Please Enter Price');
-//     } else {
-//       setProducts([...products, newProduct]);
-//       setProduct('');
-//       setPrice('');
-
-//       const intPrice = parseInt(price, 10);
-
-//       setTotal(prevTotal => prevTotal + intPrice);
-//       console.log('Current Total is: ', total);
-
-//       try {
-//         const storedProducts =
-//           JSON.parse(await AsyncStorage.getItem('products')) || [];
-//         storedProducts.push(newProduct);
-//         await AsyncStorage.setItem('products', JSON.stringify(storedProducts));
-//       } catch (error) {
-//         console.error(error);
-//       }
-//     }
-//   };
-//   const renderInputContainer = () => {
-//     return (
-//       <SafeAreaView style={styles.inputContainerStyle}>
-//         <Text style={{fontSize: 18}}>Total Grocery Till Now: {total}</Text>
-//         <View
-//           style={{
-//             flexDirection: 'row',
-//             justifyContent: 'space-around',
-//             width: '100%',
-//           }}>
-//           <TextInput
-//             style={{
-//               width: 180,
-//               height: 60,
-//               backgroundColor: 'white',
-//               borderRadius: 80,
-//               paddingHorizontal: 10,
-//               marginVertical: 20,
-//             }}
-//             onChangeText={setProduct}
-//             value={product}
-//             placeholder="Product"
-//           />
-//           <TextInput
-//             style={{
-//               width: 180,
-//               height: 60,
-//               backgroundColor: 'white',
-//               borderRadius: 80,
-//               paddingHorizontal: 10,
-//               marginVertical: 20,
-//             }}
-//             onChange={event => {
-//               const newText = event.nativeEvent.text.replace(/[^0-9]/g, '');
-//               setPrice(newText);
-//             }}
-//             value={price}
-//             placeholder="Price"
-//             keyboardType="numeric"
-//           />
-//         </View>
-
-//         <View style={styles.textButtonContainer}>
-//           <TextButton
-//             buttonText="Add to List"
-//             onPress={() => addToList()}></TextButton>
-//         </View>
-//       </SafeAreaView>
-//     );
-//   };
-//   return (
-//     <SafeAreaView style={styles.mainContainer}>
-//       {/* Header */}
-//       {renderHeader()}
-//       {/* Grocery FlatList */}
-//       {renderGroceryList()}
-//       {/* Input Container */}
-//       {renderInputContainer()}
-//     </SafeAreaView>
-//   );
-// };
-
-// export default GroceryScreen;
-
 import React, {useEffect} from 'react';
 import {
   View,
@@ -279,6 +7,7 @@ import {
   TextInput,
   Alert,
   KeyboardAvoidingView,
+  Pressable,
 } from 'react-native';
 import styles from './styles';
 import {TextButton} from '../../components/textButton';
@@ -388,20 +117,54 @@ const GroceryScreen = ({navigation}) => {
           }}
           renderItem={({item}) => {
             return (
-              <View
+              <Pressable
+                onLongPress={() =>
+                  Alert.alert('Delete Item', item.product, [
+                    {
+                      text: 'Delete',
+                      onPress: async () => {
+                        // try {
+                        //   await AsyncStorage.removeItem('products');
+                        //   return true;
+                        // } catch (exception) {
+                        //   return false;
+                        // }
+                        deleteLastEntry();
+                      },
+                      style: 'destructive',
+
+                      //Async Remove from list and total
+                    },
+                    {
+                      text: 'cancel',
+                      onPress: () => console.log('Cancelled'),
+                      style: 'cancel',
+                    },
+                  ])
+                }
                 style={{
                   flexDirection: 'row',
                   justifyContent: 'space-around',
                   alignItems: 'center',
                   paddingBottom: 5,
+                  backgroundColor: 'orange',
+                  height: 50,
                 }}>
                 <Text>{item.date}</Text>
-                <Text>{item.product}</Text>
+                <Text style={{width: 150}}>{item.product}</Text>
                 <Text>{item.price}</Text>
-              </View>
+              </Pressable>
             );
           }}
-          keyExtractor={item => item.id}
+          ItemSeparatorComponent={() => {
+            return (
+              <View
+                style={{
+                  height: 2,
+                  width: '100%',
+                }}></View>
+            );
+          }}
         />
       </View>
     );
@@ -423,7 +186,11 @@ const GroceryScreen = ({navigation}) => {
   };
   const addToList = async () => {
     // console.log("Data to be added is: ", currentDate, product, price);
-    const newProduct = {date: currentDate, product: product, price: price};
+    const newProduct = {
+      date: currentDate,
+      product: product,
+      price: price,
+    };
     if (!product) {
       Alert.alert('Please Enter Product');
     } else if (!price) {
@@ -447,6 +214,28 @@ const GroceryScreen = ({navigation}) => {
       }
     }
   };
+  const deleteLastEntry = async () => {
+    const storedProducts =
+      JSON.parse(await AsyncStorage.getItem('products')) || [];
+    if (storedProducts.length > 0) {
+      const lastProduct = storedProducts[storedProducts.length - 1];
+      storedProducts.pop(); // remove the last entry
+      setProducts(storedProducts);
+
+      let updatedTotal = total - lastProduct.price;
+      setTotal(updatedTotal);
+
+      try {
+        await AsyncStorage.setItem('products', JSON.stringify(storedProducts));
+        await AsyncStorage.setItem('total', updatedTotal.toString());
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      Alert.alert('No items to delete');
+    }
+  };
+
   const renderInputContainer = () => {
     return (
       <SafeAreaView style={styles.inputContainerStyle}>
